3c1003092a5332a750512a1e6e1131aa6664f152
Umidjon12346 <umidshari456@gmail.com>
2023-10-02 14:10:37 +0500

    solved

--- Index05.py
+++ Index05.py
@@ -1,3 +1,4 @@
+import string
 def main(s):
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
@@ -6,4 +7,6 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    digit = string.digits
+    return str(s.count(digit))
+print(main("12345"))
--- Index08.py
+++ Index08.py
@@ -6,5 +6,10 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s.count("*")==0:
+       return False
+    else:
+        return s.find("*")
+
+print(main("2144"))
         
